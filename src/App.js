d8ea13220fcd32303e0004c165617f3ee748d0ee
naraeseo <naraeseoez@gmail.com>
2022-09-26 12:35:45 +0900

    Section 12 162: 컴포넌트의 렌더링 예시

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
+import Button from './components/UI/Button/Button';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log('App Running');
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowPragraph) => !prevShowPragraph);
+  };
+
   return (
-    <div className="app">
+    <div className='app'>
       <h1>Hi there!</h1>
+      <DemoOutput show={false} />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
